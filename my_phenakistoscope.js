bf4dd908ca5bac0302893b50797dee1d0307f96c
ameliabarrett <ameliarbarrett@icloud.com>
2023-09-13 13:23:03 +1200

    final edits

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -14,22 +14,19 @@ function setup_pScope(pScope){
 }
 
 function setup_layers(pScope){
- new PLayer(null, 14, 16, 61);  //lets us draw the whole circle background, ignoring the boundaries
+ new PLayer(null, 14, 16, 61);  //dark navy background setup
 
  
- var ringFour = new PLayer(fourthRing);
- ringFour.mode(RING);
- ringFour.set_boundary(900, 1000);
 
- var ringThree = new PLayer(thirdRing);
+ var ringThree = new PLayer(thirdRing); //outermostnavy background
  ringThree.mode(RING);
- ringThree.set_boundary(750, 900);
+ ringThree.set_boundary(750, 1000);
 
- var ringTwo = new PLayer(secondRing);
+ var ringTwo = new PLayer(secondRing); //coloured centre ring
  ringTwo.mode(RING);
  ringTwo.set_boundary(600, 750);
 
- var ringOne = new PLayer(firstRing);
+ var ringOne = new PLayer(firstRing); //innermost navy background
  ringOne.mode(RING);
  ringOne.set_boundary(400, 600);
 
@@ -38,23 +35,23 @@ function setup_layers(pScope){
   lanternSequence.set_boundary( 200, 1000 );
 
 
-  var starImage = new PLayer(stars); //shaped stars rotating at top of PScope
-  starImage.mode(RING);
-  starImage.set_boundary(950,20);
+  var drawnStar = new PLayer(stars); //drawn pointed stars rotating at top of PScope
+  drawnStar.mode(RING);
+  drawnStar.set_boundary(950,20);
 
   var starsStay = new PLayer(stillStars) //still, plain white circle stars with no animation
  starsStay.mode (RING);
  starsStay.set_boundary (950, 20);
 
-  var starsColor = new PLayer(animatedStars) //colour changing circle stars
- starsColor.mode (RING);
- starsColor.set_boundary (950, 20);
+  var starsAnimate = new PLayer(animatedStars) //colour changing circle stars
+  starsAnimate.mode (RING);
+  starsAnimate.set_boundary (950, 20);
 
- var lanternImage = new PLayer(lantern); //lanterns going out
+ var lanternImage = new PLayer(lantern); //lanterns animation out from centre
  lanternImage.mode(SWIRL(8));
  lanternImage.set_boundary( 420, 1100 );
 
-  var sunImage = new PLayer(sun); // centre sun
+  var sunImage = new PLayer(sun); // centre sun spinning
   sunImage.mode(RING);
   sunImage.set_boundary(0,0);
 
@@ -62,25 +59,22 @@ function setup_layers(pScope){
 
 }
 
-function fourthRing(x, y, animation, pScope){
-  pScope.fill_background(14, 16, 61); //3, 41, 49, 60
-}
 
 function thirdRing(x, y, animation, pScope){
-  pScope.fill_background(14, 16, 61); //16, 39, 82
+  pScope.fill_background(14, 16, 61); //dark navy
 }
 
 function secondRing(x, y, animation, pScope){
-  pScope.fill_background(16, 39, 82); // 
+  pScope.fill_background(16, 39, 82); //lighter navy
 }
 
 function firstRing(x, y, animation, pScope){
-  pScope.fill_background(14, 16, 61); //10, 41, 99
+  pScope.fill_background(14, 16, 61); //dark navy
 }
 
 
 
-function stillStars(x,y,animation,pScope){
+function stillStars(x,y,animation,pScope){ //white circle stars staying stationary
   scale(animation.wave*1);
    noStroke();
    fill(255);
@@ -94,14 +88,14 @@ function stillStars(x,y,animation,pScope){
   }
 
 
-function animatedStars(x,y,animation,pScope){
+function animatedStars(x,y,animation,pScope){ //stars animating between navy and white colour
   
  let StartColour = color(14, 16, 61);  //navy
  let endColour = color(255); //white
-  let animatingColour = lerpColor(StartColour, endColour, animation.frame);
+  let animatingColour = lerpColor(StartColour, endColour, animation.frame); //colour animate
 
   translate(70, -40);
-  scale(animation.wave*50);
+  scale(animation.wave*50); //colour changing in a wave
   noStroke();
   fill(animatingColour);
   ellipse(10, 950, 15, 15); //draws circle
@@ -122,36 +116,36 @@ function animatedStars(x,y,animation,pScope){
  }
 
 
-function stars(x,y,animation,pScope){
+function stars(x,y,animation,pScope){ //pointed drawn stars animating 
  translate(220,995);
  scale(0.5);
- rotate(180*animation.frame);
+ rotate(180*animation.frame); //180* rotation animation
 
-  pScope.draw_image("stars",x,y);
+  pScope.draw_image("stars",x,y); //draws pointed stars from image
 }
 
 
 
-function lantern(x, y, animation, pScope){
+function lantern(x, y, animation, pScope){ //lanterns moving out
   scale(0.08);
-   scale(2.2*animation.frame);
+   scale(2.2*animation.frame); //growing as they move outwards
    translate(x+50, y);
-   var lanternx = animation.wave(5)*600
+   var lanternx = animation.wave(5)*600 //wiggling motion 
    pScope.draw_image("lantern",lanternx,y);
  }
 
 function release(x, y, animation, pScope){
- translate(x,y-370);
+ translate(x,y-370); //position
  scale(0.12);
- pScope.draw_image_from_sequence("lanternRelease", 0, 0, animation.frame);
+ pScope.draw_image_from_sequence("lanternRelease", 0, 0, animation.frame); //loading image sequence
 }
 
-function sun(x,y,animation,pScope){
+function sun(x,y,animation,pScope){ //centre sun spinning
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
   fill(14, 16, 61)
-  arc(x,y,500,500,backgroundArcStart,backgroundArcEnd);
+  arc(x,y,500,500,backgroundArcStart,backgroundArcEnd); //middle circle drawn
   scale(animation.frame*2);
   pScope.draw_image("sun",x,y);
 
